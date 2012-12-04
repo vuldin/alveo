28664d79b794d7f7ecb13c118226698a4c73602e
Joshua Purcell <joshua.purcell@gmail.com>
2012-12-04 10:17:42 -0800

    client/server variables working; implementing subdirs

--- web/js/server.js
+++ web/js/server.js
@@ -1,74 +1,50 @@
-var sys=require('util'),
-    http=require('http'),
-    fs=require('fs'),
-    //mediaPath='/home/joshua/media/video/episodes/Pokemon',
-    findit=require('findit'),
-    playlist=new Array(),
-    playlistContent="/* THIS IS AN AUTOMATICALLY GENERATED FILE. CONTENT WILL BE MODIFIED BY server.js */\n",
+var fs=require('fs'),
+    walk=require('walkdir');
     nowjs=require('now');
-
-var httpServer=http.createServer(function(request,response){
-  /*
-  console.log("request.url: "+request.url);
-  var result = request.url.match(/^\/(.*\.js)/);
-  console.log("result: "+result);
-  if(result) {
-      // Serve JavaScript
-      response.writeHead(200, {'Content-Type': 'text/javascript'});
-      response.write(readFile(result[1]));
-  } else if(request.url.indexOf("add?&id=") != -1){
-      // Process video_ids
-      var vals = request.url.split("=");
-      console.log(vals[vals.length-1]);
-      var playlist = fs.openSync("./playlist.csv", "a+");
-      fs.writeSync(playlist, ", '" + vals[vals.length -1 ] + "'");
-      fs.close(playlist);
-  }
-  else{
-      // Serve index.html
-      var ifile = fs.openSync("./playlist.js", "w");
-      var icontent = readFile("./playlist.csv");
-      var list = "var playlist = [" + icontent + "];\n";
-      console.log(list);
-      fs.writeSync(ifile, list);
-      fs.close(ifile);
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(readFile('index.html'));
-  }
-  */
-  //everyone.now.findFiles(mediaPath);
-  //console.log(playlist.length+" files in playlist");
-  //var ifile = fs.openSync("./playlist.js", "w");
-  //var icontent = readFile("./playlist.csv");
-  //var list = "var playlist = [" + icontent + "];\n";
-  /*
-  playlistContent+="var playlist=[";
-  for(var i=0;i<playlist.length;i++){
-    playlistContent+="'"+playlist[i]+"',";
-  }
-  playlistContent=playlistContent.substring(0,playlistContent.length-1);
-  playlistContent+="];";
-  fs.writeSync(ifile, playlistContent);
-  fs.close(ifile);
-  */
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write(fs.readFileSync('../index.html'));
-  response.end();
-  console.log("==================================================");
-});
-httpServer.listen(8888);
-console.log('Server running at http://127.0.0.1:8888/');
-
-var everyone = nowjs.initialize(httpServer);
-everyone.now.findFiles=function(path){
-  console.log(path);
-  //if(path==undefined)path=mediaPath;
-  findit.find(path,function(file){
-    playlist.push(file);
+var server=require('http').createServer(function(req,response){
+  fs.readFile('../index.html',function(err,data){
+    response.writeHead(200,{'Content-Type':'text/html'});
+    //response.writeHead(200,{'Content-Type':'video/mp4'});
+    response.write(data);
+    response.end();
   });
-  return playlist;
+});
+server.listen(8888);
+console.log('Server running at http://127.0.0.1:8888/');
+var everyone = nowjs.initialize(server);
+var options={"follow_symlinks":true,"max_depth:":3};
+var emitter=walk('../video',options);
+//everyone.now.list=new Array();
+var dirs=new Array();
+emitter.on('directory',function(dirname,stat){
+  if(dirname.indexOf("/.")==-1){
+    //console.log(dirname);
+    //console.log(stat);
+    var target=dirs;
+    for(var i=0;i<dirs.length;i++)if(dirs[i].path.indexOf(dirname))target=dirs[i].subdirs;
+    var dir={"name":dirname.substring(dirname.lastIndexOf("/")+1,dirname.length),"path":dirname,"lastmod":stat.atime,"subdirs":new Array(),"vids":new Array()};
+    target.push(dir);
+  }
+});
+//var vids=new Array();
+emitter.on('file',function(filename,stat){
+  // ignore hidden files
+  if(filename.indexOf("/.")==-1){
+    var vid={"name":filename.substring(filename.lastIndexOf("/")+1,filename.length),"path":filename,"lastmod":stat.atime};
+    //console.log(JSON.stringify(vid));
+    // find the appropriate collection 
+    for(var i=0;i<dirs.length;i++){
+      //console.log('vid.path: '+vid.path.substring(0,filename.lastIndexOf("/")));
+      //console.log('filenamesub: '+dirs[i].path);
+      if(dirs[i].path==vid.path.substring(0,filename.lastIndexOf("/"))){
+        dirs[i].vids.push(vid);
+      }
+    }
+  }
+});
+everyone.now.getList=function(){
+  console.log(dirs);
+  //return JSON.stringify(dirs);
+  //return dirs;
+  everyone.now.createDirectoryList(dirs);
 };
-everyone.now.mediaPath='/home/joshua/media/video/episodes/Pokemon';
-everyone.now.sendMessage = function(message) {
-  everyone.now.receiveMessage(message);
-}
