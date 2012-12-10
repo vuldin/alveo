09cd397ccb5e14163f33a8f37ce2000ae1346c56
Joshua Purcell <joshua.purcell@gmail.com>
2012-12-10 01:25:48 -0800

    working on updating server vids with timePlayed

--- web/js/server.js
+++ web/js/server.js
@@ -32,18 +32,6 @@ var mediaserverUrl="http://wiivid:9000/";
 var server=require('http').createServer(function(req,response){
   var path=req.url,
       contentType='';
-  /*
-  var pathext=path.substring(path.lastIndexOf('.')+1,path.length);
-  var contentTypes = {
-    'html': 'text/html',
-    'js':   'text/javascript',
-    'css':  'text/css',
-    'ico':  'image/x-icon'
-  };
-  for(ext in contentTypes){
-    if(pathext==ext)contentType=contentTypes[ext];
-  }
-  */
   if(path=='/'){
     path='/index.html';
   }
@@ -75,61 +63,134 @@ var everyone = nowjs.initialize(server);
 //var options={"follow_symlinks":true,"max_depth:":1};
 var options={"follow_symlinks":true};
 var emitter=walk('../video',options);
-var dirs=new Array();
+var top={
+ 'name':'toplevel',
+ 'path':'../video',
+ 'url':'http://wiivid:9000/',
+ 'lastmod':'',
+ 'subdirs':new Array(),
+ 'vids':new Array()
+};
 emitter.on('directory',function(dirpath,stat){
   if(dirpath.indexOf("/.")==-1){
     var vids=new Array();
-    //vids.sort(sortByPath);
-    var dir={"name":dirpath.substring(dirpath.lastIndexOf("/")+1,dirpath.length),"path":dirpath,"url":dirpath.replace(dirpath.substring(0,dirpath.indexOf("video")+6),mediaserverUrl),"lastmod":stat.atime,"subdirs":new Array(),"vids":vids};
-    //console.log(dirpath.substring(0,dirpath.lastIndexOf("/")));
-    //console.log(dirs[dirs.length].path);
-    /*
-    if(dirs[dirs.length-1]!=undefined){
-      console.log('last:\t\t'+dirs[dirs.length-1].path);
-      console.log('current:\t'+dirpath);
-      console.log('mod:\t\t'+dirpath.substring(0,dirpath.lastIndexOf("/")));
-    }
-    */
-    var parentFound=false;
-    for(var i=0;i<dirs.length;i++){
-      //if(dirs[dirs.length-1]!=undefined){
-        //console.log('mod:\t\t'+dirpath.substring(0,dirpath.lastIndexOf("/")));
-        //console.log('comparing to:\t'+dirs[i].path);
-        if(dirpath.substring(0,dirpath.lastIndexOf("/"))==dirs[i].path){
-          // parent of current directory matches an existing directory
-          console.log('subdir: '+dirpath);
-          var subdirs=dirs[i].subdirs;
-          subdirs.push(dir);
-          parentFound=true;
-        }
-      //}else dirs.push(dir);
-    }
-    if(!parentFound){
-      console.log('add: '+dirpath);
-      dirs.push(dir);
+    var subdirs=new Array();
+    var dir={"name":dirpath.substring(dirpath.lastIndexOf("/")+1,dirpath.length),"path":dirpath,"url":dirpath.replace(dirpath.substring(0,dirpath.indexOf("video")+6),mediaserverUrl),"lastmod":stat.atime,"subdirs":subdirs,"vids":vids};
+    if(dirSearch(dirpath,top.subdirs)==-1){
+      // dir not found
+      //console.log('new dir');
+      var parentPath=dir.path.substring(0,dir.path.lastIndexOf('/'));
+      var parent=dirSearch(parentPath,top.subdirs);
+      if(parent==-1){
+        // add to top
+        //console.log('this is a top level directory');
+        top.subdirs.push(dir);
+      }
+      else{
+        // add to parent
+        //console.log('adding to parent');
+        //console.log(parent.path);
+        //console.log('\t'+dir.name);
+        parent.subdirs.push(dir);
+      }
     }
   }
 });
 emitter.on('file',function(vidpath,stat){
   // ignore hidden files
   if(vidpath.indexOf("/.")==-1){
-    var vid={"name":vidpath.substring(vidpath.lastIndexOf("/")+1,vidpath.length),"path":vidpath,"url":vidpath.replace(vidpath.substring(0,vidpath.indexOf("video")+6),mediaserverUrl),"lastmod":stat.atime};
-    // find the appropriate collection
-    for(i in dirs){
-      if(dirs[i].url==vid.url.substring(0,vid.url.lastIndexOf("/"))){
-        dirs[i].vids.push(vid);
+    var vid={"name":vidpath.substring(vidpath.lastIndexOf("/")+1,vidpath.length),"path":vidpath,"url":vidpath.replace(vidpath.substring(0,vidpath.indexOf("video")+6),mediaserverUrl),"lastmod":stat.atime,'mime':mime.lookup(vidpath),'timePlayed':0};
+    if(dirSearch(vidpath,top.subdirs)==-1){
+      // dir not found
+      //console.log('new vid');
+      var parentPath=vid.path.substring(0,vid.path.lastIndexOf('/'));
+      var parent=dirSearch(parentPath,top.subdirs);
+      if(parent==-1){
+        // add to top
+        //console.log('this is a top level video');
+        top.vids.push(vid);
+      }
+      else{
+        // add to parent
+        //console.log('adding to parent');
+        //console.log(parent.path);
+        //console.log('\t'+vid.name);
+        parent.vids.push(vid);
       }
     }
   }
 });
 emitter.on('end',function(){
   console.log('dirwalk done');
-  for(i in dirs){
-    sortDir(dirs[i]);
-  }
+  sortDir(top);
+  //waitforDefined(_sVidSearch('http://wiivid:9000/episodes/Breaking Bad/Season05/Breaking Bad - s05e01 - Live Free of Die.mp4'));
+  //console.log(_sVidSearch('http://wiivid:9000/episodes/Breaking Bad/Season05/Breaking Bad - s05e01 - Live Free of Die.mp4'));
 });
+everyone.now.sUpdateVid=function(vid){
+  console.log('playing '+vid.name+' at '+val);
+  var thisVid=_sVidSearch(vid.url);
+  var parentPath=vid.path.substring(0,vid.path.lastIndexOf('/'));
+  var parent=dirSearch(parentPath,top.subdirs);
+  parent.vids.splice(parent.vids.indexOf(thisVid),0,vid); 
+};
 everyone.now.sGetList=function(){
-  everyone.now.cCreateDirectoryList(dirs);
+  everyone.now.cCreateDirectoryList(top);
+};
+function dirSearch(path,array){
+  //console.log('search request: '+path);
+  var result=-1;
+  for(var i=0;i<array.length;i++){
+    if(path.indexOf(array[i].path)>-1){
+      //console.log('partial path found');
+      if(path==array[i].path){
+        //console.log('match found');
+        result=array[i];
+        break;
+      }
+      result=dirSearch(path,array[i].subdirs);
+    }
+  }
+  //console.log(result);
+  //console.log('search result: '+result.path);
+  return result;
+}
+var results=new Array();
+function addResult(result){
+  results.push(result);
+  console.log('adding '+result.name+' (results: '+results.length+')');
+}
+
+everyone.now.sVidSearch=function(url){
+  everyone.now.cVidSearch(_sVidSearch(url));
+};
+function _sVidSearch(url){
+  var result=-1;
+  vidSearch(url);
+  //vidSearchWait();
+  for(var i=0;i<results.length;i++){
+    //console.log(results[i]);
+    if(results[i]!=undefined)result=results[i];
+  }
+  return result;
+}
+function vidSearch(url,array){
+  //setSearchCount(getSearchCount()+1);
+  if(array==undefined)array=top.subdirs;
+  //console.log('vid search: '+url);
+  dirloop:for(var i=0;i<array.length;i++){
+    //console.log('partial url found');
+    vidloop:for(var j=0;j<array[i].vids.length;j++){
+      if(url==array[i].vids[j].url){
+        //console.log('match found: '+array[i].vids[j].name);
+        addResult(array[i].vids[j]);
+      }
+      if(results.length>0)break vidloop;
+    }
+    //console.log('searching next levels down from '+array[i].url);
+    if(results.length>0)break dirloop;
+    vidSearch(url,array[i].subdirs);
+  }
+  //setSearchCount(getSearchCount()-1);
 };
 function sortDir(dir){
   dir.subdirs.sort(function(a,b){
@@ -142,3 +203,25 @@ function sortDir(dir){
     return a.path.localeCompare(b.path);
   });
 }
+/*
+var searchCount=0;
+function getSearchCount(){
+  return searchCount;
+}
+function setSearchCount(num){
+  console.log('search: from '+searchCount+' to '+num);
+  searchCount=num;
+}
+function vidSearchWait(){
+  if(getSearchCount()>0){
+    console.log('waiting on vidSearch');
+    setTimeout(vidSearchWait, 200);
+  }
+}
+function waitforDefined(variable){
+  if(variable==undefined){
+    console.log('waiting on '+variable);
+    setTimeout(waitforDefined, 1000);
+  }
+}
+*/
