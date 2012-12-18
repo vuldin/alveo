e30d8a65501be507245521a23115bc75103ffd79
Joshua Purcell <joshua.purcell@gmail.com>
2012-12-18 02:14:01 -0800

    recent vids view, updating lastmod and found on vids

--- web/js/server.js
+++ web/js/server.js
@@ -75,18 +75,19 @@ var chunksize=(end-start)+1;
 response.writeHead(206,{'Content-Range':'bytes '+start+'-'+end+'/'+total,'Accept-Ranges':'bytes','Content-Length':chunksize,'Content-Type':type});
 response.end(file.slice(start,end),'binary'); 
 */
+var options={"follow_symlinks":true};
+var emitter=walk('../video',options);
 var everyone = nowjs.initialize(server);
-var top=null;
+var data=null;
 var recent=new Array();
+var vidSearchResults=new Array();
 //var options={"follow_symlinks":true,"max_depth:":1};
 if(typeof localStorage==='undefined'||localStorage===null){
   var LocalStorage=require('node-localstorage').LocalStorage;
   localStorage=new LocalStorage('./scratch');
 }
-if(!localStorage.getItem('top')){
-  var options={"follow_symlinks":true};
-  var emitter=walk('../video',options);
-  top={
+if(!localStorage.getItem('data')){
+  data={
    'name':'toplevel',
    'path':'../video',
    'url':'http://wiivid:9000/',
@@ -94,91 +95,16 @@ if(!localStorage.getItem('top')){
    'subdirs':new Array(),
    'vids':new Array()
   };
-  emitter.on('directory',function(dirpath,stat){
-    if(dirpath.indexOf("/.")==-1){
-      var vids=new Array();
-      var subdirs=new Array();
-      var dir={
-          'name':   dirpath.substring(dirpath.lastIndexOf('/')+1,dirpath.length),
-          'path':   dirpath,
-          'url':    dirpath.replace(dirpath.substring(0,dirpath.indexOf('video')+6),mediaserverUrl),
-          'lastmod':stat.atime,
-          'subdirs':subdirs,
-          'vids':   vids
-          };
-      if(dirSearch(dirpath,top.subdirs)==-1){
-        // dir not found
-        //console.log('new dir');
-        var parentPath=dir.path.substring(0,dir.path.lastIndexOf('/'));
-        var parent=dirSearch(parentPath,top.subdirs);
-        if(parent==-1){
-          // add to top
-          //console.log('this is a top level directory');
-          top.subdirs.push(dir);
-        }
-        else{
-          // add to parent
-          //console.log('adding to parent');
-          //console.log(parent.path);
-          //console.log('\t'+dir.name);
-          parent.subdirs.push(dir);
-        }
-      }
-    }
-  });
-  emitter.on('file',function(vidpath,stat){
-    // ignore hidden files
-    if(vidpath.indexOf('/.')==-1){
-      var vid={
-          'name':vidpath.substring(vidpath.lastIndexOf('/')+1,vidpath.lastIndexOf('.')),
-          'path':vidpath,
-          'url':vidpath.replace(vidpath.substring(0,vidpath.indexOf('video')+6),mediaserverUrl),
-          'lastmod':stat.atime,
-          'mime':mime.lookup(vidpath),
-          'timePlayed':0,
-          'omdb':-1, // will be !-1 if one value is returned from omdb
-          'omdbResults':-1, // will be !-1 if multiple values are returned from omdb
-          'omdbError':-1,
-          'metascore':-1
-          };
-      if(dirSearch(vidpath,top.subdirs)==-1){
-        // dir not found
-        //console.log('new vid');
-        var parentPath=vid.path.substring(0,vid.path.lastIndexOf('/'));
-        var parent=dirSearch(parentPath,top.subdirs);
-        if(parent==-1){
-          // add to top
-          //console.log('this is a top level video');
-          top.vids.push(vid);
-        }
-        else{
-          // add to parent
-          //console.log('adding to parent');
-          //console.log(parent.path);
-          //console.log('\t'+vid.name);
-          parent.vids.push(vid);
-        }
-      }
-    }
-  });
-  emitter.on('end',function(){
-    console.log('dirwalk done');
-    sortDir(top);
-    console.log(JSON.stringify(top,null,2));
-    localStorage.setItem('top',JSON.stringify(top));
-    //waitforDefined(_sVidSearch('http://wiivid:9000/episodes/Breaking Bad/Season05/Breaking Bad - s05e01 - Live Free or Die.mp4'));
-    //omdbFindVid(_sVidSearch('http://wiivid:9000/episodes/Breaking Bad/Season05/Breaking Bad - s05e01 - Live Free or Die.mp4'));
-    //omdbFindVid(_sVidSearch('http://wiivid:9000/lane/The Adventures Of Walker And Ping Ping - The Chinese Market (2008).mp4'));
-    //omdbFindVid(_sVidSearch('http://wiivid:9000/lane/Journey To The East - The River Dragon King (2008)/Journey To The East - The River Dragon King (2008) - pt1.mp4'));
-    //omdbFindVid(_sVidSearch('http://wiivid:9000/action/Inglourious Basterds (2009).mp4'));
-  });
 }else{
   //localStorage._deleteLocation();
-  top=JSON.parse(localStorage.getItem('top'));
+  data=JSON.parse(localStorage.getItem('data'));
   //omdbFindVid(_sVidSearch('http://wiivid:9000/episodes/Breaking Bad/Season05/Breaking Bad - s05e01 - Live Free or Die.mp4'));
   //omdbFindVid(_sVidSearch('http://wiivid:9000/action/Star Trek (2009).mp4'));
   //omdbFindVid(_sVidSearch('http://wiivid:9000/action/Inglourious Basterds (2009).mp4'));
+  //omdbFindVid(vidSearch('http://localhost:9000/The Legend of Korra/The Legend of Korra - S01E03 - The Revelation.mp4'));
+  //omdbFindVid(vidSearch('http://wiivid:9000/The Legend of Korra/The Legend of Korra - S01E03 - The Revelation.mp4'));
 }
+resetFound();
 if(!localStorage.getItem('recent')){
   recent={
       'vids':new Array()
@@ -186,59 +112,164 @@ if(!localStorage.getItem('recent')){
 }else{
   recent=JSON.parse(localStorage.getItem('recent'));
 }
+emitter.on('directory',function(dirpath,stat){
+  if(dirpath.indexOf("/.")==-1){ // only non-hidden files
+    var dir=dirSearch(dirpath,data.subdirs);
+    if(dir==-1){ // only if it doesn't already exist in data
+      var vids=new Array();
+      var subdirs=new Array();
+      var dir={
+          'name':   dirpath.substring(dirpath.lastIndexOf('/')+1,dirpath.length),
+          'path':   dirpath,
+          'url':    dirpath.replace(dirpath.substring(0,dirpath.indexOf('video')+6),mediaserverUrl),
+          'lastmod':stat.atime,
+          'found':  true,
+          'subdirs':subdirs,
+          'vids':   vids
+      };
+      var parentPath=dir.path.substring(0,dir.path.lastIndexOf('/'));
+      var parent=dirSearch(parentPath,data.subdirs);
+      if(parent==-1){
+        // add to data
+        //console.log('this is a top level directory');
+        data.subdirs.push(dir);
+      }
+      else{
+        // add to parent
+        //console.log('adding to parent');
+        //console.log(parent.path);
+        //console.log('\t'+dir.name);
+        parent.subdirs.push(dir);
+      }
+    }else{
+      dir.found=true;
+      dir.lastmod=stat.atime;
+      /*
+      // this always was true anyway...
+      if(dir.lastmod!=stat.atime){
+        console.log('updating lastmod for dir: '+dir.name);
+      }
+      */
+    }
+  }
+});
+emitter.on('file',function(vidpath,stat){
+  // ignore hidden files
+  if(vidpath.indexOf('/.')==-1){
+    var vid=vidSearch(vidpath.replace(vidpath.substring(0,vidpath.indexOf('video')+6),mediaserverUrl));
+    if(vid==-1){
+      console.log('new vid: '+vidpath);
+      vid={
+          'name':vidpath.substring(vidpath.lastIndexOf('/')+1,vidpath.lastIndexOf('.')),
+          'path':vidpath,
+          'url':vidpath.replace(vidpath.substring(0,vidpath.indexOf('video')+6),mediaserverUrl),
+          'lastmod':stat.atime,
+          'found':true,
+          'mime':mime.lookup(vidpath),
+          'timePlayed':0,
+          'duration':0,
+          'omdb':-1, // will be !-1 if one value is returned from omdb
+          'omdbResults':-1, // will be !-1 if multiple values are returned from omdb
+          'omdbError':-1,
+          'metascore':-1
+          };
+      if(dirSearch(vidpath,data.subdirs)==-1){ // dir not found
+        var parentPath=vid.path.substring(0,vid.path.lastIndexOf('/'));
+        var parent=dirSearch(parentPath,data.subdirs);
+        if(parent==-1){
+          console.log('this is a top level video');
+          data.vids.push(vid);
+        }
+        else parent.vids.push(vid);
+      }
+    }else{
+      vid.found=true;
+      var newmod=new Date(stat.atime);
+      if(vid.lastmod!=newmod.toISOString()){
+        console.log('updating lastmod for vid: '+vid.name);
+        vid.lastmod=stat.atime;
+      }
+    }
+  }
+});
+emitter.on('end',function(){
+  console.log('fswalk done');
+  sortDir(data);
+  //console.log(JSON.stringify(data,null,2));
+  localStorage.setItem('data',JSON.stringify(data));
+  //waitforDefined(_sVidSearch('http://wiivid:9000/episodes/Breaking Bad/Season05/Breaking Bad - s05e01 - Live Free or Die.mp4'));
+  //omdbFindVid(_sVidSearch('http://wiivid:9000/episodes/Breaking Bad/Season05/Breaking Bad - s05e01 - Live Free or Die.mp4'));
+  //omdbFindVid(_sVidSearch('http://wiivid:9000/lane/The Adventures Of Walker And Ping Ping - The Chinese Market (2008).mp4'));
+  //omdbFindVid(_sVidSearch('http://wiivid:9000/lane/Journey To The East - The River Dragon King (2008)/Journey To The East - The River Dragon King (2008) - pt1.mp4'));
+  //omdbFindVid(_sVidSearch('http://wiivid:9000/action/Inglourious Basterds (2009).mp4'));
+});
+function resetFound(array){ // sets all dirs/vids found variable to false
+  if(array==undefined)array=data.subdirs;
+  $.each(array,function(dirkey,dir){
+    dir.found=false;
+    $.each(dir.vids,function(vidkey,vid){
+      vid.found=false;
+    });
+    resetFound(dir.subdirs);
+  });
+}
 function omdbFindVid(vid){
-  if(vid.omdb==-1&&vid.omdbResults==-1&&vid.omdbError==-1){
-    // vid has no omdb metadata
+  // TODO need to handle not finding the vid
+  console.log('omdbFindVid');
+  console.log(vid);
+  if(vid.omdb==-1&&vid.omdbResults==-1&&vid.omdbError==-1){ // vid has no omdb metadata yet
     var series='';
     var title='';
     var year='';
     var reqUrl='';
-    if(vid.path.match(/\s-\s[Ss][0-9]{2,3}[Ee][0-9]{2,3}\s-\s/)){
-      // is in episodes folder (name is [series] - [num] - title)
+    if(vid.path.match(/\s-\s[Ss][0-9]{2,3}[Ee][0-9]{2,3}\s-\s/)){ // is in episodes folder (name is [series] - [num] - title)
+      console.log('episode');
       series=vid.name.substring(0,vid.name.indexOf('-')-1);
       title=vid.name.substring(vid.name.lastIndexOf('-')+2,vid.name.length);
     }
-    if(vid.name.substring(vid.name.lastIndexOf(')')-4,vid.name.lastIndexOf(')')).match(/^\d{4}$/)!=null){
-      // name contains a year
+    if(vid.name.substring(vid.name.lastIndexOf(')')-4,vid.name.lastIndexOf(')')).match(/^\d{4}$/)!=null){ // name contains a year
+      console.log('contains year');
       title=vid.name.substring(0,vid.name.lastIndexOf('(')-1);
       year=vid.name.substring(vid.name.lastIndexOf(')')-4,vid.name.lastIndexOf(')'));
     };
-    if(title.match(/\s-\s/)){
-      // split title with series
+    if(title.match(/\s-\s/)){ // split title with series
+      console.log('contains series');
       series=title.substring(0,title.indexOf(' -'));
       title=title.substring(title.indexOf(' - ')+3,title.length);
     }
-    /*
     console.log('|'+series+'|');
     console.log('|'+title+'|');
     console.log('|'+year+'|');
-    */
     reqUrl='http://www.omdbapi.com/?s='+series+'&t='+title+'&y='+year+'&tomatoes=true&callback=?';
     //reqUrl='http://www.omdbapi.com/?s='+series+'&t='+title+'&y='+year+'&callback=?';
     console.log(reqUrl);
-    $.getJSON(reqUrl,function(data){
-      if(data.Error){
+    $.getJSON(reqUrl,function(searchResult){
+      if(searchResult.Error){
         console.log('error or no results');
-        vid.omdbError=data;
+        vid.omdbError=searchResult;
         //everyone.now.sUpdateVid(vid);
-        localStorage.setItem('top',JSON.stringify(top));
+        localStorage.setItem('searchResult',JSON.stringify(searchResult));
       }
-      if(data.Search){
-        console.log('multiple results ('+data.Search.length+')');
-        vid.omdbResults=data.Search;
+      if(searchResult.Search){
+        console.log('multiple results ('+searchResult.Search.length+')');
+        vid.omdbResults=searchResult.Search;
         //everyone.now.sUpdateVid(vid);
-        localStorage.setItem('top',JSON.stringify(top));
+        localStorage.setItem('searchResult',JSON.stringify(searchResult));
       }
-      if(data.Title){
+      if(searchResult.Title){
         console.log('single result');
-        vid.omdb=data;
+        vid.omdb=searchResult;
         //everyone.now.sUpdateVid(vid);
-        localStorage.setItem('top',JSON.stringify(top));
+        localStorage.setItem('searchResult',JSON.stringify(searchResult));
       }
     });
   }else{
     // vid has omdb metadata
-    if(vid.omdb!=-1)console.log('local result: '+vid.omdb.Plot);
+    if(vid.omdb!=-1){
+      console.log('local result');
+      console.log(vid.omdb);
+      //console.log('local result: '+vid.omdb.Plot);
+    }
     if(vid.omdbResults!=-1){
       console.log('multiple local results');
       console.log(vid.omdbResults);
@@ -252,16 +283,17 @@ function omdbFindVid(vid){
 everyone.now.sBackup=function(vid){
   //console.log(vid.name+': '+vid.timePlayed);
   //console.log(vid.url);
-  var currentVid=_sVidSearch(vid.url);
+  var currentVid=vidSearch(vid.url);
   var parentPath=vid.path.substring(0,vid.path.lastIndexOf('/'));
-  var parent=dirSearch(parentPath,top.subdirs);
+  var parent=dirSearch(parentPath,data.subdirs);
   //var index=parent.vids.indexOf(currentVid);
   //console.log(index+'/'+parent.vids.length);
   //console.log(JSON.stringify(parent.vids[index],null,2));
   parent.vids.splice(parent.vids.indexOf(currentVid),1,vid);
   //console.log('length: '+parent.vids.length);
   //console.log(JSON.stringify(parent.vids,null,2));
-  localStorage.setItem('top',JSON.stringify(top));
+  localStorage.setItem('data',JSON.stringify(data));
+  everyone.now.cGetData(data);
 };
 everyone.now.sAddRecent=function(vidurl){
   //var match=false;
@@ -280,15 +312,19 @@ everyone.now.sAddRecent=function(vidurl){
   //}
   //console.log(recent);
   localStorage.setItem('recent',JSON.stringify(recent));
+  this.now.cGetRecent(recent);
 };
 everyone.now.sGetRecent=function(){
-  //console.log('recent: '+JSON.stringify(recent,null,2));
-  everyone.now.cGetRecent(recent);
+  console.log('sGetRecent from: '+this.user.clientId);
+  this.now.cGetRecent(recent);
 }
 everyone.now.sGetData=function(){
-  //callback only to the client that sent the request
-  everyone.now.cGetData(top);
+  console.log('sGetRecent from: '+this.user.clientId);
+  this.now.cGetData(data);
 };
+everyone.now.sGetMediaServerUrl=function(){
+  this.now.cGetMediaServerUrl(mediaserverUrl);
+}
 function dirSearch(path,array){
   //console.log('search request: '+path);
   var result=-1;
@@ -303,56 +339,36 @@ function dirSearch(path,array){
       result=dirSearch(path,array[i].subdirs);
     }
   }
-  //console.log(result);
   //console.log('search result: '+result.path);
   return result;
 }
-var results=new Array();
-function addResult(result){
-  results.push(result);
-  //console.log('adding '+result.name+' (results: '+results.length+')');
-}
 everyone.now.sVidSearch=function(url){
-  everyone.now.cVidSearch(_sVidSearch(url));
+  console.log('sVidSearch from: '+this.user.clientId);
+  //everyone.now.cVidSearch(vidSearch(url));
+  this.now.cVidSearch(vidSearch(url));
 };
-function _sVidSearch(url,array){
-  /*
-   * created as a wrapper to the vidSearch function to handle an array of results
-   */
-  results=new Array();
+function vidSearch(url,array){
+  //vidSearchResults=new Array();
   var result=-1;
-  vidSearch(url,array);
-  //vidSearchWait();
-  for(var i=0;i<results.length;i++){
-    //console.log(results[i]);
-    if(results[i]!=undefined)result=results[i];
-  }
+  _vidSearch(url,array);
+  $.each(vidSearchResults,function(key,val){
+    if(val!=undefined)result=val;
+  });
   return result;
 }
-function vidSearch(url,array){
-  //var result=-1;
-  if(array==undefined)array=top.subdirs;
-  //console.log('vid search: '+url);
-  dirloop:for(var i=0;i<array.length;i++){
-    if(url.indexOf(array[i].url)>-1){
-      //console.log('partial match');
-      vidloop:for(var j=0;j<array[i].vids.length;j++){
-        //console.log('currentVid: '+array[i].vids[j].name);
-        if(url==array[i].vids[j].url){
-          //console.log('match found: '+array[i].vids[j].name);
-          //result=array[i].vids[j];
-          addResult(array[i].vids[j]);
-        }
-        //if(results.length>0)break vidloop;
-        //if(result!=-1)break vidloop;
-      }
-      //console.log('searching for subdirs of '+array[i].url);
-      //if(results.length>0)break dirloop;
-      //if(results!=-1)break dirloop;
-      vidSearch(url,array[i].subdirs);
+function _vidSearch(url,array){
+  if(array==undefined)array=data.subdirs;
+  $.each(array,function(dirkey,dir){
+    if(url.indexOf(dir.url)>-1){
+      $.each(dir.vids,function(vidkey,vid){
+        if(url==vid.url)addVidResult(vid);
+      });
+      vidSearch(url,dir.subdirs);
     }
-  }
-  //if(result!=-1&&result!=undefined)return result;
+  });
+}
+function addVidResult(val){
+  vidSearchResults.push(val);
 }
 function sortDir(dir){
   dir.subdirs.sort(function(a,b){
@@ -365,25 +381,3 @@ function sortDir(dir){
     return a.path.localeCompare(b.path);
   });
 }
-/*
-var searchCount=0;
-function getSearchCount(){
-  return searchCount;
-}
-function setSearchCount(num){
-  console.log('search: from '+searchCount+' to '+num);
-  searchCount=num;
-}
-function vidSearchWait(){
-  if(getSearchCount()>0){
-    console.log('waiting on vidSearch');
-    setTimeout(vidSearchWait, 200);
-  }
-}
-function waitforDefined(variable){
-  if(variable==undefined){
-    console.log('waiting on '+variable);
-    setTimeout(waitforDefined, 1000);
-  }
-}
-*/
